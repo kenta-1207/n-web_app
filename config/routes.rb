f98a32a2282ad05a1742dc23b569d2e2222280c5
kenta-1207 <ktnstn.237rogs@gmail.com>
2021-07-09 21:52:40 +0900

    letter_opener修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener_web" if Rails.env.development?
   resources :contacts
   devise_for :users
   root to: "tops#index"
