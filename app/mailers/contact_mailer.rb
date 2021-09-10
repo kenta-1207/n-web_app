9cf8c9a9304bf625cfc8374a9aab6caa0ae4e8db
kenta-1207 <ktnstn.237rogs@gmail.com>
2021-09-10 20:03:54 +0900

    syntaxError修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
     @user = User.find(params[:id])
-    if page_select_id = 1, 
+    if page_select_id = 1
       mail to:"ktnstn.237rogs@gmail.com",subject: "confirmation"
     else
       mail to:"tessin1396@icloud.com",subject: "confirmation"
