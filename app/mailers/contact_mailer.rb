5477ab7df36aa9b3e87ffa56825dba86069943a4
kenta-1207 <ktnstn.237rogs@gmail.com>
2021-09-10 20:12:28 +0900

    mailer修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    @user = User.find(params[:id])
-    if page_select_id = 1
-      mail to:"ktnstn.237rogs@gmail.com",subject: "confirmation"
-    else
-      mail to:"tessin1396@icloud.com",subject: "confirmation"
-    end
+    mail to:"ktnstn.237rogs@gmail.com",subject: "confirmation"
   end
 end
