ef6c6f2ea6633a36dd74e7e4b6c03d48f6dc129a
kenta-1207 <ktnstn.237rogs@gmail.com>
2021-09-11 09:52:09 +0900

    メール内容の修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    mail to:"ktnstn.237rogs@gmail.com",subject: "confirmation"
+    mail to:"ktnstn.237rogs@gmail.com",subject: "サンプル依頼が届きました。"
   end
 end
