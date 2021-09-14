9e10f8f1cbd6827b6bbe3bc85b89b8964b3670ad
kenta-1207 <ktnstn.237rogs@gmail.com>
2021-09-14 21:17:03 +0900

    メール元通りへ

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,13 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
-    @contact = contact(contact_params)
-    if @contact.contact_user_belong_id == 1
-      mail to:"ktnstn.237rogs@gmail.com",subject: "サンプル依頼が届きました。"
-    if @contact.contact_user_belong_id == 2
-      mail to:"tessin1396@icloud.com",subject: "サンプル依頼が届きました。"
-    else
-      render :root_path
-    end
-    end
+    @contact = contact
+    mail to:"ktnstn.237rogs@gmail.com",subject: "サンプル依頼が届きました。"
   end
 end
