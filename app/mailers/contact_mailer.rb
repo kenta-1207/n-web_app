a0b7c070f372d451d7aeb0f72c2fef18841da939
kenta-1207 <ktnstn.237rogs@gmail.com>
2021-09-14 21:12:24 +0900

    メール修正

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
-    @contact = contact
+    @contact = contact(contact_params)
     if @contact.contact_user_belong_id == 1
       mail to:"ktnstn.237rogs@gmail.com",subject: "サンプル依頼が届きました。"
     if @contact.contact_user_belong_id == 2
