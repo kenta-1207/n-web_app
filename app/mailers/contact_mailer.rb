60c53fc0f39d48bfc3072af8e2895d80565b58bb
kenta-1207 <ktnstn.237rogs@gmail.com>
2021-09-16 19:35:07 +0900

    contact_user_belong_id消去

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,6 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-    if @contact.contact_user_belong_id == 1
       mail to:"ktnstn.237rogs@gmail.com",subject: "サンプル依頼が届きました。"
-    else
-      mail to:"tessin1396@icloud.com",subject: "サンプル依頼が届きました。"
-    end
   end
 end
