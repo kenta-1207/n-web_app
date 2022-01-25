15de2b9d805b70530e91740340bba962a4667837
kenta-1207 <ktnstn.237rogs@gmail.com>
2022-01-25 20:44:51 +0900

    試しの設定

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,10 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-      mail to:"ktnstn.237rogs@gmail.com",subject: "サンプル依頼が届きました。"
+      if @contact.page_select_id == 1
+        mail to:"ktnstn.237rogs@gmail.com",subject: "サンプル依頼が届きました。"
+      else
+        mail to:"k-watanabe@net-nakayama.co.jp",subject: "成功"
+      end
   end
 end
