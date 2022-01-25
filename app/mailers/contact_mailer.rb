25058919c9bb631233b8074ad59f94c06950839a
kenta-1207 <ktnstn.237rogs@gmail.com>
2022-01-25 21:01:28 +0900

    これはどう

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,9 @@
 class ContactMailer < ApplicationMailer
   def contact_mail(contact)
     @contact = contact
-      if @contact.user.page_select_id == 1
+      if contact.user.page_select_id == 1
         mail to:"ktnstn.237rogs@gmail.com",subject: "サンプル依頼が届きました。"
-      elsif @contact.user.page_select_id == 2
+      elsif contact.user.page_select_id == 2
         mail to:"k-watanabe@net-nakayama.co.jp",subject: "成功"
       else
         mail to:"tessin1396@icloud.com",subject: "完璧！"
